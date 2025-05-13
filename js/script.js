539be67f814a570dd5885705aa2fcf340265ce4f
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2025-05-13 11:34:31 +0700

    Make an Responsif Interface

--- js/script.js
+++ js/script.js
@@ -1,37 +1,45 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const form = document.getElementById("contactForm");
 
-//     form.addEventListener("submit", function(event) {
-//         const name = document.getElementById("name").value.trim();
-//         const email = document.getElementById("email").value.trim();
-//         const destination = document.getElementById("destination").value;
-
-//         if (name === "" || email === "" || destination === "") {
-//             alert("Please fill in all fields.");
-//             event.preventDefault();
-//         } else if (!/^[a-zA-Z\s]+$/.test(name)) {
-//             alert("Name must contain only letters.");
-//             event.preventDefault();
-//         }
-//     });
-// });
+const popup = document.getElementById('popup');
+const openBtn = document.querySelector('.button-action');
+const exitBtn = document.querySelector('.exit-btn');
+const form = document.getElementById("contactForm");
 
 
-  const popup = document.getElementById('popup');
-  const openBtn = document.querySelector('.button-action');
-  const exitBtn = document.querySelector('.exit-btn');
+openBtn.addEventListener('click', () => {
+  popup.style.display = 'flex';
+});
 
-  openBtn.addEventListener('click', () => {
-    popup.style.display = 'flex';
-  });
-
-  exitBtn.addEventListener('click', () => {
-    popup.style.display = 'none';
-  });
+exitBtn.addEventListener('click', () => {
+  popup.style.display = 'none';
+});
 
 
-  popup.addEventListener('click', (e) => {
-    if (e.target === popup) {
-      e.stopPropagation(); 
+popup.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    e.stopPropagation(); 
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  form.addEventListener("submit", function (event) {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const destination = document.getElementById("destination").value;
+
+    if (name === "" || email === "" || destination === "") {
+      alert("Please fill in all fields.");
+      event.preventDefault(); 
+    } else if (!/^[a-zA-Z\s]+$/.test(name)) {
+      alert("Name must contain only letters.");
+      event.preventDefault();
+    } else {
+      
+      console.log("Form yang diisi dengan value:");
+      console.log("Nama:", name);
+      console.log("Email:", email);
+      console.log("Tujuan:", destination);
+
+      event.preventDefault();
     }
   });
+});
