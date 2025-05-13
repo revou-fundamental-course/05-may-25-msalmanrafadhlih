c04a16bad4327b84a58a296e1a00bd7dd359de31
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2025-05-13 18:17:06 +0700

    Fix Responsif & add .JS

--- js/script.js
+++ js/script.js
@@ -11,28 +11,38 @@ openBtn.addEventListener('click', () => {
 
 exitBtn.addEventListener('click', () => {
   popup.classList.remove('show');
+
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("destination").value = "";
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Mencegah form submit langsung
+
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
     const destination = document.getElementById("destination").value;
 
     if (name === "" || email === "" || destination === "") {
       alert("Please fill in all fields.");
-      event.preventDefault(); 
     } else if (!/^[a-zA-Z\s]+$/.test(name)) {
       alert("Name must contain only letters.");
-      event.preventDefault();
     } else {
+      const confirmed = confirm("Apakah anda yakin dengan data ini?");
+      if (confirmed) {
+        console.log("Form yang diisi dengan value:");
+        console.log("Nama:", name);
+        console.log("Email:", email);
+        console.log("Tujuan:", destination);
       
-      console.log("Form yang diisi dengan value:");
-      console.log("Nama:", name);
-      console.log("Email:", email);
-      console.log("Tujuan:", destination);
-
-      event.preventDefault();
+        form.reset();
+        popup.classList.remove('show');
+        alert("Data anda berhasil tersimpan! hehehe..")
+      }
     }
   });
+
 });
+
